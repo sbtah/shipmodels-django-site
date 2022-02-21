b2c8c521995d184527b01dc487216a66e7f5f2e4
Sbtah <shisastore@protonmail.com>
2022-02-21 17:39:13 +0100

    Added urls for order views in admin panel

--- panel/urls.py
+++ panel/urls.py
@@ -9,4 +9,12 @@ urlpatterns = [
     path('', views.main_panel_view, name='panel'),
     path('login/', views.LoginCustomUserView.as_view(), name='login'),
     path('logout/', views.LogoutCustomUserView.as_view(), name='logout'),
+    path('order-list/', views.OrderListView.as_view(), name='order-list'),
+    path('order-detail/<int:pk>/',
+         views.OrderDetailView.as_view(), name='order-detail'),
+    path('order-update/<int:pk>/',
+         views.OrderUpdateView.as_view(), name='order-update'),
+    path('order-delete/<int:pk>/',
+         views.OrderDeleteView.as_view(), name='order-delete')
+
 ]
