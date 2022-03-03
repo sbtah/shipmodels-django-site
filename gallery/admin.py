62be096ca63921f915d5dc1a89d35255819d40a9
Sbtah <shisastore@protonmail.com>
2022-03-03 18:49:49 +0100

    Added ImageGalleryAdmin that uses model form in admin site. Just for tests, user wont have access to django admin site

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from gallery.models import ImagePost, ImageGallery
+from gallery.forms import ImageGalleryForm
 
+
+class ImageGalleryAdmin(admin.ModelAdmin):
+    """Custom model admin that utilizes ImageGalleryForm."""
+    form = ImageGalleryForm
+
+
+admin.site.register(ImageGalleryAdmin)
 admin.site.register(ImagePost)
 admin.site.register(ImageGallery)
