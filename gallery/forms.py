4067bec264282501089e1b545f3691cd8170af37
Sbtah <shisastore@protonmail.com>
2022-03-03 18:41:26 +0100

    Added custom __init__ method on ImageGalleryForm

--- gallery/forms.py
+++ gallery/forms.py
@@ -6,6 +6,14 @@ from django.utils.translation import gettext_lazy as _
 class ImageGalleryForm(forms.ModelForm):
     """Model form for ImageGallery object."""
 
+    def __init__(self, *args, **kwargs):
+        """Custom __init__ that applies css classes to form style."""
+        super().__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs.update(
+            {'class': 'form__input form__label'})
+        self.fields['posts'].widget.attrs.update(
+            {'class': 'form__input form__label'})
+
     def clean_posts(self):
         """Custom clean """
         posts = self.cleaned_data['posts']
