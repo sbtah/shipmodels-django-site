dfb4b450c71dbca91a7c7fcf0138459f58f665ae
Sbtah <shisastore@protonmail.com>
2022-03-05 15:22:43 +0100

    Added test_forms_clean_method that checks that order is not created with bad phone number.

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -1,6 +1,7 @@
 import pytest
 from orders.models import Order
 from orders.forms import OrderForm
+from django.core.exceptions import ValidationError
 
 
 pytestmark = pytest.mark.django_db
@@ -9,7 +10,7 @@ pytestmark = pytest.mark.django_db
 class TestOrderForm():
     """Test case for OrderForm."""
 
-    def test_form_object_created(self, client):
+    def test_form_object_created(self):
         """Test that object is created through form."""
 
         order_data = {
@@ -25,3 +26,19 @@ class TestOrderForm():
         form.is_valid()
         form.save()
         assert Order.objects.all().count() == 1
+
+    def test_forms_clean_method(self):
+        """Test that data cannot be saved with bad phone number"""
+
+        order_data = {
+            'imię_i_nazwisko': 'Test Tester',
+            'numer_telefonu': -1,
+            'email': 'test@test.com',
+            'model': 'Statek',
+            'komentarz': 'Do it now!',
+        }
+        form = OrderForm(data=order_data)
+        assert Order.objects.all().count() == 0
+        assert not form.is_valid()
+        with pytest.raises(Exception) as error:
+            form.save()
