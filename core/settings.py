6b806a1454b57d76b8cff10e66b780624206a993
Sbtah <shisastore@protonmail.com>
2022-03-27 14:33:10 +0200

    Modified settings.puy to server static files.

--- core/settings.py
+++ core/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media settings
 MEDIA_URL = '/media/'
